be9c52b04c3bde60cac7acef231466e9f5c1b241
VladimirZ00Z <94064126+VladimirZ00Z@users.noreply.github.com>
2021-11-16 17:42:35 +0300

    Add files via upload

---
+++ RimToArab/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace RimToArab
+{
+    class Program
+    {
+        static void Main()
+        {
+            string text = Console.ReadLine();
+            char[] arr = text.ToCharArray();
+            int sum = 0;
+            Dictionary<char, int> rimDg = new Dictionary<char, int> {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+            };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += rimDg[arr[i]];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
