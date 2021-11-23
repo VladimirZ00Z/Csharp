81b6352c9efe0f3cea2427a98ce25773350f25b3
VladimirZ00Z <94064126+VladimirZ00Z@users.noreply.github.com>
2021-11-23 17:23:29 +0300

    Add files via upload

---
+++ book/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace book
+{
+    class Book : IComparable<Book>
+    {
+        private string title;
+        private string author;
+        private int edition;
+
+        public Book(string t, string a, int e)
+        {
+            title = t;
+            author = a;
+            edition = e;
+        }
+
+        public string Title { get { return title; } }
+        public string Author { get { return author; } }
+        public int Edition { get { return edition; } }
+
+        public int CompareTo(Book b)
+        {
+            if (author.CompareTo(b.Author) != 0)
+                return author.CompareTo(b.Author);
+            if (title.CompareTo(b.Title) != 0)
+                return title.CompareTo(b.Title);
+
+            return edition.CompareTo(b.Edition);
+        }
+    }
+}
---
+++ book/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book a = new Book("To Kill a Mockingbird", "Harper Lee", 13);
+            Book b = new Book("CLR via C#", "Jeffrey Richter", 4);
+            Console.WriteLine(a.CompareTo(b));
+        }
+    }
+}
